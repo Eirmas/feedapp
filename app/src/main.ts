15d554c0c57c8ef006e835eb18711eac723d43d6
Eirik Måseidvåg <eirik.maaseidvaag@gmail.com>
2023-10-10 20:42:28 +0200

    Workflow

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './plugins/router/router';
+import router from './plugins/router';
 import pinia from './store';
 import i18n from './plugins/i18n/i18n';
 import './index.scss';
