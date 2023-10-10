430583536864344dcb6a8ee5246bf45e298609a8
Eirik Måseidvåg <eirik.maaseidvaag@gmail.com>
2023-10-11 00:48:53 +0200

    Fix tests

--- app/src/views/index/Index.vue
+++ app/src/views/index/Index.vue
@@ -12,6 +12,7 @@ const login = () => {
   supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
+      redirectTo: window.location.origin,
       queryParams: {
         access_type: 'offline',
         prompt: 'consent',
